af5bbefd1845024882cfd30eca522ee4bd488821
Tomás Henrique Batista Aguiar <tomas.h.aguiar@accenture.com>
2021-09-23 18:05:29 -0300

    new repo first commit

---
+++ DojoHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrackNTraceDojo
+{
+    public class DojoHandler
+    {
+        public static List<string> GetNames() => new List<string>
+            {"Tomás", "Lucas", "Samuel", "Henrique", "Saulo", "Caio"};
+    }
+
+    public class RandomNames
+    {
+        public Guid Id;
+        public string Name;
+    }
+
+    public static class Sort
+    {
+        public static List<string> Randomize(List<string> namesList)
+        {
+            List<string> list;
+            do
+            {
+                var names = new List<RandomNames>();
+                namesList.ForEach(x => names.Add(new RandomNames {Id = Guid.NewGuid(), Name = x}));
+                list = names.OrderBy(i => i.Id).Select(x => x.Name).ToList();
+            } while (!list.Equals(namesList));
+            
+            return list;
+        }
+    }
+}
---
+++ TrackNTraceDojoTests/DojoHandlerTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using TrackNTraceDojo;
+using Xunit;
+
+namespace TrackNTraceDojoTests
+{
+    public class DojoHandlerTest
+    {
+        [Fact]
+        public void GetsTheListOfNames()
+        {
+            var result = DojoHandler.GetNames();
+
+            Assert.Equal(7, result.Count);
+        }
+
+        [Fact]
+        public void GetListOfRandomNames()
+        {
+            var names = new List<string>
+            {
+                "name1",
+                "name2",
+                "name3"
+            };
+            var namesList = Sort.Randomize(names);
+
+            Assert.NotEqual(names, namesList);
+        }
+    }
+}
